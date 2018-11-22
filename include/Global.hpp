8c810ce8d64d28ecedc6b7f1a2201cf1c2cad518
Eyosyas Dagnachew <eyosyaswd@outlook.com>
2018-11-22 13:05:36 -0500

    Forgot to commit all edited files in the last commit... oops. Also added all other files that we've been using except for Debug because that's specific to each machine.

---
+++ include/Global.hpp
@@ -0,0 +1,19 @@
+/**
+  * Filename: Global.hpp
+  *
+  * Global class.
+  * Used to define constants and call on libraries.
+**/
+
+
+#ifndef GLOBAL_HPP
+#define GLOBAL_HPP
+
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define BITS_PER_PIXEL 16
+#define GAME_TITLE "COLLISION"
+
+
+#endif
